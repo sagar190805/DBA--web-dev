b7ada58c028dc74092417f58fa56a68be073a7b4
Sagar Chand <chandsagar496@gmail.com>
2026-06-30 18:53:55 +0530

    some higher order function  practice

---
+++ JS/hof.js
@@ -0,0 +1,18 @@
+// let hof(()=>{})
+    let num = [1,2,3,4,5,6,7,8,9,10]
+    let num2 = num.map((i)=>{
+          return i*2})
+          console.log(num2);
+          // map((value,index,array)=>{
+            // })
+            ////// filter////////////  3 parameter(val,ind,arr)
+            let num3= num.filter((val,ind,arr)=>{
+                return val>6
+            })
+            console.log(num3)
+
+            let num4=num.reduce((acc,val)=>{// (4 parameter  previous value, current val, current index,array {}, initial value)
+                return acc+val
+            },0)
+            console.log(num4);
+            
