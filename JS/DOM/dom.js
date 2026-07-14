7d9e0aab458748d964aaf4a739ebe514f83f98b6
Sagar Chand <chandsagar496@gmail.com>
2026-07-14 15:24:40 +0530

    added some css and settimer in clock

--- JS/DOM/dom.js
+++ JS/DOM/dom.js
@@ -7,8 +7,14 @@ function clock(){
     let minutes=date.getMinutes();
     let seconds=date.getSeconds();
     h1.innerHTML=`${hours}:${minutes}:${seconds}`;
-    h1.innerHTML=`${hours}:${minutes}:${seconds}`;
 }
 div.appendChild(h1);
 document.body.appendChild(div);
 clock();
+setInterval(clock,1000);
+div.style.height='500px';
+div.style.width='1300px';
+div.style.color='red';
+div.style.display='flex';
+div.style.justifyContent='center';
+div.style.alignItems='center';
